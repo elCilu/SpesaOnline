39291a79ebfe82cbc91e81704c1068d1279128ee
Gianni Narducci <g.narducci99@gmail.com>
2020-05-09 12:32:38 +0200

    Ho aggiunto un commento del cazzo :)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,6 +14,8 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
+        // AGGIUNTA COMMENTO DEL CAZZO PER FARVI CAPIRE
     }
 
 
