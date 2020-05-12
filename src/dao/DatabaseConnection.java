2d2d42be7d9f04129d984e34da20c814ad51b7a7
Gianni Narducci <g.narducci99@gmail.com>
2020-05-12 10:54:06 +0200

    Aggiunta classe DatabaseConnection per creare connessione al database da java. Da scaricare e aggiungere al classpath il mssql driver (vedi TODO list su github "MSSQL Driver"). Una volta modificata la connectionUrl inserite il file nel .gitignore

---
+++ src/dao/DatabaseConnection.java
@@ -0,0 +1,27 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    // Connect to your database.
+    // Replace server name, username, and password with your credentials
+    public static void main(String[] args) {
+        String connectionUrl =
+                "jdbc:sqlserver://localhost:1433;"
+                        + "database=spesaonline;" //nome db su mssql
+                        + "user=SA;" //username di mssql
+                        + "password=_secret_1234;" //vostra password al mssql
+                        + "loginTimeout=30;";
+
+        try (Connection connection = DriverManager.getConnection(connectionUrl)) {
+            System.out.println("You did it!");
+        }
+        // Handle any errors that may have occurred.
+        catch (SQLException e) {
+            System.out.println("You failed it :(");
+            e.printStackTrace();
+        }
+    }
+}
