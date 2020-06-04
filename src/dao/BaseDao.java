6a72dc2b50e5c03db7468237560d6b006109c04b
HL99s <65026011+HL99s@users.noreply.github.com>
2020-06-04 21:48:01 +0200

    Connessione db

--- src/dao/BaseDao.java
+++ src/dao/BaseDao.java
@@ -7,15 +7,18 @@ import java.sql.SQLException;
 abstract class BaseDao {
     //TODO: for every DAO class: print db status (executing query, returned x rows, etc.)
     static Connection connection = createConnection();
-    private static final String connectionUrl =
-            "jdbc:jtds:sqlserver://LAPTOP-BNV4BFEE/ProgettoSpesa;instance=MSSQLSERVER;"
+    private static final String connectionUrl = "jdbc:sqlserver://LAPTOP-BNV4BFEE;databaseName=ProgettoSpesa;user=HL99;password=admin";
+            /*"jdbc:jtds:sqlserver://LAPTOP-BNV4BFEE/ProgettoSpesa;instance=MSSQLSERVER;"
+                    + "database=ProgettoSpesa;"
                     + "user=HL99;" //username di mssql
-                    + "password=Huluigi99;"; //vostra password al mssql
+                    + "password=admin;" //vostra password al mssql
+                    + "loginTimeout=30;";*/
 
     protected static Connection createConnection() {
 
         try {
             System.out.print("Connecting to database...");
+            System.setProperty("java.net.preferIPv6Addresses", "true");
             connection = DriverManager.getConnection(connectionUrl);
             System.out.println("Connected to database!");
         } catch (SQLException e) {
