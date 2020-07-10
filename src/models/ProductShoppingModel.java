591d5a63ef802450a025c73a8a839c3eeba61654
HL99s <65026011+HL99s@users.noreply.github.com>
2020-07-10 17:09:42 +0200

    Controllo totale

--- src/models/ProductShoppingModel.java
+++ src/models/ProductShoppingModel.java
@@ -2,9 +2,9 @@ package models;
 
 public class ProductShoppingModel {
     private int id;
-    private final int idProduct;
-    private final int idShopping;
-    private final int qty;
+    private int idProduct;
+    private int idShopping;
+    private int qty;
 
     public ProductShoppingModel(int id, int idProduct, int idShopping, int qty){
         this.id = id;
@@ -17,17 +17,34 @@ public class ProductShoppingModel {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
     public int getIdProduct() {
         return idProduct;
     }
+
+    public void setIdProduct(int idProduct) {
+        this.idProduct = idProduct;
+    }
+
 
     public int getIdShopping() {
         return idShopping;
     }
 
+    public void setIdShopping(int idShopping) {
+        this.idShopping = idShopping;
+    }
+
+
     public int getQty() {
         return qty;
     }
 
-
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
 }
